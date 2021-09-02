bcc8e6c489688f4dbbdebebc36e5c96e2f87fdb6
vladko-89 <63239096+vladko-89@users.noreply.github.com>
2021-09-02 17:10:47 +0400

    fix: styles NavBar points

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -22,18 +22,18 @@ const NavBar = () => {
         <div className="mr-15">
             <ul className={`${styles.container} mt-0 mb-20`}>
                 <li className={`${styles.item}`}>
-                    <NavLink to='/profile' className={`${styles.link} text text_type_main-default text_color_inactive pt-5 pb-5`} activeClassName={`${location.pathname === '/profile' && styles.active}`}>Профиль</NavLink>
+                    <NavLink to='/profile' className={`${styles.link} text text_type_main-medium text_color_inactive pt-5 pb-5`} activeClassName={`${location.pathname === '/profile' && styles.active}`}>Профиль</NavLink>
                 </li>
                 <li className={styles.item}>
-                    <NavLink to='/profile/orders' className={`${styles.link} text text_type_main-default text_color_inactive pt-5 pb-5`} activeClassName={styles.active}>История заказов</NavLink>
+                    <NavLink to='/profile/orders' className={`${styles.link} text text_type_main-medium text_color_inactive pt-5 pb-5`} activeClassName={styles.active}>История заказов</NavLink>
                 </li>
                 <li className={styles.item}>
-                    <button  className={`${styles.button} text text_type_main-default text_color_inactive pt-5 pb-5`} onClick={handleExit}>Выход</button>
+                    <button  className={`${styles.button} text text_type_main-medium text_color_inactive pt-5 pb-5`} onClick={handleExit}>Выход</button>
                 </li>
             </ul>
         {location.pathname === '/profile' 
         ?
-            <p className={`${styles.text} text text_type_main-small text_color_inactive`}>В этом разделе вы можете изменить свои персональные данные</p>
+            <p className={`${styles.text} text text_type_main-default text_color_inactive`}>В этом разделе вы можете изменить свои персональные данные</p>
         :
             <></>
         
